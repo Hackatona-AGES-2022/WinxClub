8e32fca59f76fd22c2e6d715b75c5324ed0639e1
aSchemitt <alison.schemitt@edu.pucrs.br>
2022-05-28 11:48:59 -0300

    arrumando destaque

--- src/main/java/com/WinxClub/Imclusion/negocio/Destaque.java
+++ src/main/java/com/WinxClub/Imclusion/negocio/Destaque.java
@@ -5,30 +5,45 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
+import com.WinxClub.Imclusion.models.ModeloImigrante;
+import com.WinxClub.Imclusion.repositories.IRepositorioImigrante;
+import com.WinxClub.Imclusion.services.ServicoImigrante;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/destaque")
 public class Destaque {
+    final ServicoImigrante s;
+    List<ModeloImigrante> destaques;
     
-    List<Imigrante> destaques = new ArrayList<>();
 
-    
-    public void atualizaDestaques(List<Imigrante> todos){
-        int lim = todos.size();
-        Random r = new Random();
-        HashSet<Integer> numeros = new HashSet<>();
-        int aux;
-        for(int i=0;i<5;){
-            aux = r.nextInt(lim);
-            if(!numeros.contains(aux)){
-                numeros.add(aux);
-                i++;
-            }
-        }
-
+    public Destaque(ServicoImigrante s) {
+        this.s = s;
+        destaques = s.findAll();
     }
 
-    public List<Imigrante> getDestaques(List<Imigrante> todos){
-        atualizaDestaques(todos);
-        return destaques;
+
+    public List atualizaDestaques(List<ModeloImigrante> destaques){
+        int lim = destaques.size();
+        List<ModeloImigrante> l;
+        if(lim-5>=0){
+            System.out.println(destaques.subList(lim-5, lim));
+            l= destaques.subList(lim-5, lim);
+        }else{
+            System.out.println(destaques.subList(0, lim));
+            l= destaques.subList(0, lim);
+        }
+        return l;
+    }
+
+    @GetMapping("/lista")
+    public List<ModeloImigrante> getDestaques(){
+        return atualizaDestaques(destaques);
+        
         
     }
 }
