1b978b081d5a947d21e088a2e0061326114eaa3b
Giovanni Schenato <gischenato@gmail.com>
2022-05-28 03:56:05 -0300

    primeiro commit

---
+++ src/main/java/com/WinxClub/Imclusion/ImclusionApplication.java
@@ -0,0 +1,13 @@
+package com.WinxClub.Imclusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImclusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImclusionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WinxClub/Imclusion/ImclusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WinxClub.Imclusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImclusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
